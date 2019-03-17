4d776382676f42524634ca4f2441d35115d6d49d
Volodymyr Zhdanov <wight554@gmail.com>
2019-03-17 17:03:25 +0000

    Revert "msm8998-common: add qcom early boot script"
    This reverts commit eb21ad5e119b9bbf54002d03cf3f154aaca69058.

--- msm8998.mk
+++ msm8998.mk
@@ -307,9 +307,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     init.panel_info.sh \
-    init.qcom.early_boot.sh \
-    init.qcom.post_boot.sh \
-    init.qcom.sh
+    init.qcom.sh \
+    init.qcom.post_boot.sh
 
 # RCS
 PRODUCT_PACKAGES += \
