e0e4dcf56a15b0364e8a9f758ff259779d12b0f2
Volodymyr Zhdanov <wight554@gmail.com>
2019-05-08 11:14:15 +0000

    Revert "Revert "msm8998-common: Do not build with BOARD_VNDK_VERSION := current""
    * ded again
    
    This reverts commit 738bda7b92e4bbe380592575bcc58666db5e8b09.
    
    Change-Id: I35b5c779f832e65cc438e7ba2410652dc522feca

--- extract-files.sh
+++ extract-files.sh
@@ -65,30 +65,10 @@ function blob_fixup() {
     vendor/etc/permissions/qcrilhook.xml)
         sed -i 's|/system/framework/qcrilhook.jar|/vendor/framework/qcrilhook.jar|g' "${2}"
         ;;
-    vendor/lib/libFaceGrade.so)
-        patchelf --remove-needed "libandroid.so" "${2}"
-        ;;
-    vendor/lib/libarcsoft_beauty_shot.so)
-        patchelf --remove-needed "libandroid.so" "${2}"
-        ;;
-    vendor/lib/libmmcamera2_stats_modules.so)
-        patchelf --remove-needed "libandroid.so" "${2}"
-        ;;
-    vendor/lib/libmpbase.so)
-        patchelf --remove-needed "libandroid.so" "${2}"
-        ;;
-    vendor/lib64/lib-dplmedia.so)
-        patchelf --remove-needed "libmedia.so" "${2}"
-        ;;
-    vendor/lib/soundfx/libdirac.so)
-        patchelf --remove-needed "libmedia.so" "${2}"
-        ;;
     vendor/bin/mlipayd@1.1)
-        patchelf --remove-needed "libandroid_runtime.so" "${2}"
         patchelf --remove-needed "vendor.xiaomi.hardware.mtdservice@1.0.so" "${2}"
         ;;
     vendor/lib64/libmlipay@1.1.so)
-        patchelf --remove-needed "libandroid_runtime.so" "${2}"
         patchelf --remove-needed "vendor.xiaomi.hardware.mtdservice@1.0.so" "${2}"
         ;;
     esac
